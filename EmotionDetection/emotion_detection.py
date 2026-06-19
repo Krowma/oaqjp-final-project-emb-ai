9dcdf8e13fbde9d8c620cb535d77408423638e98
Krowma <antoine.david.n7@gmail.com>
2026-06-19 10:29:43 -0400

    packaging

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    """analyse the text passed as parameter and return the associated emotion"""
+    
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict' 
+    myobj = { "raw_document": { "text": text_to_analyze } } 
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} 
+    response = requests.post(url, json = myobj, headers=header)
+    
+    formatted_response = json.loads(response.text)
+    emotions_result = formatted_response['emotionPredictions'][0]['emotion']
+
+    highest_score = 0
+    dominant_emotion = None
+    for label, score in emotions_result.items():
+        if(score > highest_score):
+            dominant_emotion = label
+            highest_score = score
+    
+    emotions_result['dominant_emotion'] = dominant_emotion
+
+    return emotions_result
+
